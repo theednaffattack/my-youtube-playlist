dbe0d34b5b5a72417295acb5ef197dd8e70ec503
eddie naff <eddienaff@eddienaff.com>
2020-08-19 13:28:27 -0700

    Add new Linaria components to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
+import { Container } from "../components/container";
+import { Main } from "../components/main";
+import { TitleHeaderOne } from "../components/title-header-one";
+import { TitleAnchor } from "../components/title-anchor";
+import { Description } from "../components/description";
+import { Code } from "../components/code";
+import { Grid } from "../components/grid";
+import { Card } from "../components/card";
+import { CardParagraph } from "../components/card-paragraph";
+import { CardHeaderThree } from "../components/card-header-three";
+import { GridImage } from "../components/grid-image";
+import { Footer } from "../components/footer";
+import { FooterAnchor } from "../components/footer-anchor";
+import { FooterImage } from "../components/footer-image";
 
 const YOUTUBE_PLAYLIST_ITEMS_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
@@ -17,235 +31,81 @@ export async function getServerSideProps() {
 
 export default function Home({ data }) {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <NextSeo
+        title="Using More of Config"
+        description="This example uses more of the available config options."
+        canonical="https://www.canonical.ie/"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://www.example.ie/og-image-01.jpg",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+            },
+            {
+              url: "https://www.example.ie/og-image-02.jpg",
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+            },
+            { url: "https://www.example.ie/og-image-03.jpg" },
+            { url: "https://www.example.ie/og-image-04.jpg" },
+          ],
+          site_name: "SiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Container>
+        <Main>
+          <TitleHeaderOne>Welcome to a Random YouTube Playlist</TitleHeaderOne>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <Grid>
+            {data.items.map(({ id, snippet = {} }) => {
+              const { title, thumbnails = {}, resourceId = {} } = snippet;
+              const { medium } = thumbnails;
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+              return (
+                <Card key={id}>
+                  <TitleAnchor
+                    href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
+                  >
+                    <CardParagraph>
+                      <GridImage
+                        width={medium.width}
+                        height={medium.height}
+                        src={medium.url}
+                        alt=""
+                      />
+                    </CardParagraph>
+                    <CardHeaderThree>{title}</CardHeaderThree>
+                  </TitleAnchor>
+                </Card>
+              );
+            })}
+          </Grid>
+        </Main>
+        <Footer>
+          <p>Powered by Eddie</p>
+          <p> & </p>
 
-        <ul className="grid">
-          {data.items.map(({ id, snippet = {} }) => {
-            const { title, thumbnails = {}, resourceId = {} } = snippet;
-            const { medium } = thumbnails;
-            return (
-              <li key={id} className="card">
-                <a
-                  href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
-                >
-                  <p>
-                    <img
-                      width={medium.width}
-                      height={medium.height}
-                      src={medium.url}
-                      alt=""
-                    />
-                  </p>
-                  <h3>{title}</h3>
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+          <FooterImage src="/zeit.svg" alt="ZEIT Logo" />
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: flex-start;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-
-          list-style: none;
-          padding: 0;
-          margin-left: 0;
-        }
-
-        .grid img {
-          max-width: 100%;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+          <FooterAnchor
+            href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></FooterAnchor>
+        </Footer>
+      </Container>
+    </>
   );
 }
